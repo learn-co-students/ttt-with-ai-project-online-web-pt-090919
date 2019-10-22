2da99a7273b3d85a5269be4080a00c1bb1dc0f1c
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-22 02:39:40 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,11 +1,27 @@
 module Players
   class Computer < Player
 
-    def move(location)
-      location = location.to_i
-      position = cells[location - 1]
-      taken = position(location) == "X" || position(location) == "O"
-      location.between?(1,9) && !taken
+    def move(board)
+      if board.cells[4] == " "
+        "5"
+      elsif board.cells[0] == " "
+        "1"
+      elsif board.cells[2] == " "
+        "3"
+      elsif board.cells[6] == " "
+        "7"
+      elsif board.cells[8] == " "
+        "9"
+      elsif board.cells[1] == " "
+        "2"
+      elsif board.cells[3] == " "
+        "4"
+      elsif board.cells[5] == " "
+        "6"
+      elsif board.cells[7] == " "
+        "8"
+      end
     end
+
   end
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,15 +1,9 @@
 module Players
   class Human < Player
 
-    # def move(index, player)
-    #   @board[index] = player
-    # end
+    def move(board)
+      input = gets.strip
+    end
+
   end
 end
-
-# class Player::Human < Player
-#   def move(board, timer = 0)
-#     puts "Please enter 1-9:"
-#     gets.chomp
-#   end
-# end”
